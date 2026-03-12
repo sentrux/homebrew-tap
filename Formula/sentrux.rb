ccfa3cb0071a45f88805dd6dc28248f891e80191
yjing <yjing@sentrux.dev>
2026-03-12 11:59:25 -0700

    Update sentrux to v0.2.1

--- Formula/sentrux.rb
+++ Formula/sentrux.rb
@@ -1,20 +1,20 @@
 class Sentrux < Formula
   desc "Live codebase visualization and structural quality gate"
-  homepage "https://sentrux.dev"
-  version "0.1.2"
+  homepage "https://github.com/sentrux/sentrux"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/sentrux/sentrux/releases/download/v0.1.2/sentrux-darwin-arm64"
-      sha256 "4627f9a6864187d53617f7124e463c047b1f87a223168a9e187b30f61cd097ad"
+      url "https://github.com/sentrux/sentrux/releases/download/v0.2.1/sentrux-darwin-arm64"
+      sha256 "36a5f56145af63457f333838b5db83371bc3e1f157842ecf7d35a65c1af5a898"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/sentrux/sentrux/releases/download/v0.1.2/sentrux-linux-x86_64"
-      sha256 "67a179ba6277a9c2e56be1b0000beef03a4a132866e1a6e54ef030327b4a55e2"
+      url "https://github.com/sentrux/sentrux/releases/download/v0.2.1/sentrux-linux-x86_64"
+      sha256 "8bb7e88018592b75d5da7498230675755bc8d2fadaf2c88ce3a2501ccab4cfd6"
     end
   end
 
