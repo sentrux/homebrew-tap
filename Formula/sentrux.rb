8b77fe6e7d6892868c34a5fa59813f9b0036aef8
yjing <yjing@sentrux.dev>
2026-03-11 20:32:10 -0700

    Update formula to v0.1.1

--- Formula/sentrux.rb
+++ Formula/sentrux.rb
@@ -1,20 +1,20 @@
 class Sentrux < Formula
   desc "Live codebase visualization and structural quality gate"
   homepage "https://sentrux.dev"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/sentrux/sentrux/releases/download/v0.1.0/sentrux-darwin-arm64"
-      sha256 "a6acc264f7dd212ee4d3e32b6a4a6b210991307251a14f5edab2ea8256255ccf"
+      url "https://github.com/sentrux/sentrux/releases/download/v0.1.1/sentrux-darwin-arm64"
+      sha256 "0e773f13119ee644c610462bfc9c4f9f05230551b5cf2e923982b18067bc67bd"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/sentrux/sentrux/releases/download/v0.1.0/sentrux-linux-x86_64"
-      sha256 "62af42657149846cbb152548d99ed1c35d97672008ea978157f1b23b27740644"
+      url "https://github.com/sentrux/sentrux/releases/download/v0.1.1/sentrux-linux-x86_64"
+      sha256 "fca65057bc711b3c816e0d028bdfaa37702ca793227a61617f14d4cacd9201da"
     end
   end
 
