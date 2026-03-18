d966502561e5d0b0993af381ea3bb2ff7e3a4794
yjing <yjing@sentrux.dev>
2026-03-18 15:06:26 +0000

    Update sentrux to v0.5.6

--- Formula/sentrux.rb
+++ Formula/sentrux.rb
@@ -7,14 +7,14 @@ class Sentrux < Formula
   on_macos do
     on_arm do
       url "https://github.com/sentrux/sentrux/releases/download/v0.5.6/sentrux-darwin-arm64"
-      sha256 ""
+      sha256 "5e6a5a0e547c6c767bbd0633da42d9cd6a4de462cd255e23bb998b040aee9488"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/sentrux/sentrux/releases/download/v0.5.6/sentrux-linux-x86_64"
-      sha256 ""
+      sha256 "e84d3b0e46e761ac07a4098936646ade579d1ad5160b956e528089442dcfaea1"
     end
   end
 
